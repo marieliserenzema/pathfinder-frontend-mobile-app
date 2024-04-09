69bf745feee5ce099555193220e162823e8bf468
Marie Lise <marieliserenzema@hotmail.fr>
2024-04-09 12:02:21 +0200

    add pages

--- App.tsx
+++ App.tsx
@@ -1,20 +1,49 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import CommunityScreen from './app/community';
+import AccountScreen from './app/account';
+import SavedScreen from './app/saved';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Feather } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import ExploreScreen from './app/explore';
+import { AntDesign } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+       <Tab.Navigator screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: string;
+
+            if (route.name === "Explore") {
+              iconName = "find";
+            } else if (route.name === 'Community') {
+              iconName = "hearto";
+            } else if (route.name === 'Saved') {
+              iconName = "book";
+            } else if (route.name === 'Account') {
+              iconName = "user";
+            } else {
+              iconName = '';
+            }
+            return <AntDesign name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'green',
+          inactiveTintColor: 'gray',
+        }}
+       >
+        <Tab.Screen name="Explore" component={ExploreScreen} />
+        <Tab.Screen name="Community" component={CommunityScreen} />
+        <Tab.Screen name="Saved" component={SavedScreen} />
+        <Tab.Screen name="Account" component={AccountScreen} />
+      </Tab.Navigator>
+
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ app/saved.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function SavedScreen() {
+  return (
+    <View>
+        <Text>SavedPage</Text>
+    </View>
+  );
+}
